5d9576bad5f5e9c9e80665d478b27355c2225641
odhyp <pradhana.odhy@gmail.com>
2024-12-15 17:03:36 +0700

    feat: add generate_date_range

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 from playwright.sync_api import sync_playwright
 
 
@@ -27,33 +28,63 @@ class StockBot:
         if self.playwright:
             self.playwright.stop()
 
-    def download_file(self, date):
+    def download_file(self, dates: list):
         self.page.goto(self.URL)
 
-        form_date = self.page.locator("input[name='date']")
-        form_date.wait_for()
-        form_date.click()
-        form_date.type(date)
-        form_date.press("Enter")
+        for date in dates:
+            # Date form
+            form_date = self.page.locator("input[name='date']")
+            form_date.wait_for()
+            form_date.click()
+            form_date.clear()
+            form_date.type(date)
+            form_date.press("Enter")
 
-        stock_table = self.page.locator("#vgt-table")
-        stock_table.wait_for()
+            # Table
+            stock_table = self.page.locator("#vgt-table")
+            stock_table.wait_for()
 
-        with self.page.expect_download() as download_info:
+            # Download button
             btn_download = self.page.locator('button:has-text("Unduh")')
-            btn_download.wait_for()
-            btn_download.click()
 
-        download_name = f"stock_{date}.xlsx"
-        download_file = download_info.value
-        download_file.save_as(download_name)
+            if btn_download.is_enabled():
+                with self.page.expect_download() as download_info:
+                    btn_download.wait_for()
+                    btn_download.click()
+
+                download_name = f"stock_{date}.xlsx"
+                download_file = download_info.value
+                download_file.save_as(download_name)
+
+            elif btn_download.is_disabled():
+                continue
+
+            else:
+                continue
+
+
+def generate_date_range(start_date: str, end_date: str) -> list:
+    formatted_start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+    formatted_end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+
+    date_range_length = (formatted_end_date - formatted_start_date).days + 1
+
+    date_list = [
+        (formatted_start_date + datetime.timedelta(days=i)).strftime("%Y-%m-%d")
+        for i in range(date_range_length)
+    ]
+
+    return date_list
 
 
 def main():
-    date = "2024-12-12"
+    start_date = "2024-01-01"
+    end_date = "2024-01-05"
+
+    dates = generate_date_range(start_date, end_date)
 
     with StockBot() as bot:
-        bot.download_file(date)
+        bot.download_file(dates)
 
 
 if __name__ == "__main__":

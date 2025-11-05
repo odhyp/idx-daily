14d9aef62a8dfb6909cf3708b226d8ce434f6490
odhyp <pradhana.odhy@gmail.com>
2025-11-05 14:27:07 +0700

    refactor: replace main.py with app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+"""
+IDX Daily
+
+Author: Odhy Pradhana
+License: MIT License
+Year: 2025
+"""
+
+import logging
+
+from src.logging_setup import setup_logging
+from src.menu import run_menu
+
+setup_logging()
+log = logging.getLogger(__name__)
+
+
+def run():
+    log.debug("Starting IDX Daily...")
+    run_menu()
+    log.debug("Goodbye!")
+
+
+if __name__ == "__main__":
+    run()

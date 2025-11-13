856a568efab5a4e010220253cca6c79f0c464208
odhyp <pradhana.odhy@gmail.com>
2025-11-13 20:00:48 +0700

    style: remove extra whitespaces

--- src/date_utils.py
+++ src/date_utils.py
@@ -16,12 +16,9 @@ def validate_date(date_str: str, fmt: str = "%Y-%m-%d") -> bool:
 def generate_date_range(start_date: str, end_date: str) -> list:
     formatted_start_date = datetime.strptime(start_date, "%Y-%m-%d")
     formatted_end_date = datetime.strptime(end_date, "%Y-%m-%d")
-
     date_range_length = (formatted_end_date - formatted_start_date).days + 1
-
     date_list = [
         (formatted_start_date + timedelta(days=i)).strftime("%Y-%m-%d")
         for i in range(date_range_length)
     ]
-
     return date_list

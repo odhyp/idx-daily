aafa4dad491ef2d881bb50b0d381d85fe98fc8f7
odhyp <pradhana.odhy@gmail.com>
2025-11-13 10:27:39 +0700

    feat: add menu.py

---
+++ src/menu.py
@@ -0,0 +1,29 @@
+"""
+Menu UI and App logic
+"""
+
+import logging
+from datetime import datetime
+
+from InquirerPy.prompts.confirm import ConfirmPrompt
+from InquirerPy.separator import Separator
+from rich.console import Console
+
+
+log = logging.getLogger(__name__)
+console = Console()
+
+
+def main_menu():
+    """Display main menu and return the selected option."""
+    pass
+
+
+def handle_selection(choice: str):
+    """Handle what happens after a selection."""
+    pass
+
+
+def run_menu():
+    """Menu runner."""
+    pass

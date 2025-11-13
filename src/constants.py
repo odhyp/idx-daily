fb7a7e7b474c38b99e398434413af251a509155c
odhyp <pradhana.odhy@gmail.com>
2025-11-13 19:53:59 +0700

    feat: add APP_VERSION

--- src/constants.py
+++ src/constants.py
@@ -7,6 +7,6 @@ import tomllib
 with open("config.toml", "rb") as f:
     cfg = tomllib.load(f)
 
-
+APP_VERSION = "v0.0.1"
 CHROME_DIR = cfg["base"]["chrome_dir"]
 BASE_URL = cfg["base"]["base_url"]
